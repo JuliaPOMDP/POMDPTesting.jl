e1a1106ddad6524f76b13dc407fff059d76a93ae
rejuvyesh <mail@rejuvyesh.com>
2018-08-17 10:40:23 -0700

    exports

--- src/POMDPTesting.jl
+++ src/POMDPTesting.jl
@@ -2,6 +2,11 @@ module POMDPTesting
 
 using POMDPs
 
+export test_solver,
+    probability_check,
+    obs_prob_consistency_check,
+    trans_prob_consistency_check
+
 include("model.jl")
 include("solver.jl")
 
