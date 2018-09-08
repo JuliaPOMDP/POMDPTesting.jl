719773b79b5071ef07343f2abe22de791c4ca61c
rejuvyesh <mail@rejuvyesh.com>
2018-09-07 17:24:45 -0700

    docs, travis

---
+++ docs/make.jl
@@ -0,0 +1,12 @@
+using Documenter
+using POMDPTesting
+
+makedocs(
+    format =:html,
+    sitename = "POMDPTesting.jl"
+)
+
+deploydocs(
+    repo = "github.com/JuliaPOMDP/POMDPTesting.jl.git",
+    julia = "1.0"
+)
---
+++ docs/src/index.md
@@ -0,0 +1,11 @@
+# About
+
+POMDPTesting is a collection of tools to test various solvers for [POMDPs.jl](https://github.com/JuliaPOMDP/POMDPs.jl).
+
+```@contents
+```
+
+## Index
+
+```index
+```
