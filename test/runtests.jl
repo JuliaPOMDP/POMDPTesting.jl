3d3d932b2f42615a14375e63e85e7cd2ce70e754
lassepe <lasse.peters@tuhh.de>
2019-09-17 20:02:33 -0700

    Fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,17 +3,23 @@ using POMDPs
 using POMDPTesting
 using POMDPModelTools
 
-import POMDPs: transition, observation, initialstate_distribution, updater
+import POMDPs:
+    transition,
+    observation,
+    initialstate_distribution,
+    updater,
+    states,
+    actions,
+    observations
 
 struct TestPOMDP <: POMDP{Bool, Bool, Bool} end
 updater(problem::TestPOMDP) = DiscreteUpdater(problem)
 initialstate_distribution(::TestPOMDP) = BoolDistribution(0.0)
 transition(p::TestPOMDP, s::Bool, a::Bool) = BoolDistribution(0.5)
 observation(p::TestPOMDP, a::Bool, sp::Bool) = BoolDistribution(0.5)
+states(p::TestPOMDP) = (true, false)
+actions(p::TestPOMDP) = (true, false)
+observations(p::TestPOMDP) = (true, false)
 @testset "model" begin
     POMDPTesting.probability_check(TestPOMDP())
 end
-
-
-
-
