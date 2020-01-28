962ea2d031ad851dfdb8402499f736c1a23b1fd9
Zachary Sunberg <sunbergzach@gmail.com>
2020-01-28 04:45:06 -0700

    New tests (#7)
    * New, better tests that fix #3 and fix #4
    
    * added docstrings
    
    * fixed tests
    
    * info to warn
    
    * watn -> warn

--- docs/src/model.md
+++ docs/src/model.md
@@ -1,7 +1,8 @@
 # Model
 
 ```@docs
-probability_check
-obs_prob_consistency_check
-trans_prob_consistency_check
+has_consistent_distributions
+has_consistent_initial_distribution
+has_consistent_transition_distributions
+has_consistent_observation_distributions
 ```
--- test/runtests.jl
+++ test/runtests.jl
@@ -20,6 +20,15 @@ observation(p::TestPOMDP, a::Bool, sp::Bool) = BoolDistribution(0.5)
 states(p::TestPOMDP) = (true, false)
 actions(p::TestPOMDP) = (true, false)
 observations(p::TestPOMDP) = (true, false)
+
 @testset "model" begin
+    m = TestPOMDP()
+    @test has_consistent_initial_distribution(m)
+    @test has_consistent_transition_distributions(m)
+    @test has_consistent_observation_distributions(m)
+    @test has_consistent_distributions(m)
+end
+
+@testset "old model" begin
     probability_check(TestPOMDP())
 end
