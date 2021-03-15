72a82c88519311348446119c78f93cd2d9d2ecb2
Zachary Sunberg <sunbergzach@gmail.com>
2021-03-15 15:04:57 -0600

    added note that test_solver does not test for optimality JuliaPOMDP/POMDPs.jl#339

--- src/solver.jl
+++ src/solver.jl
@@ -64,6 +64,8 @@ Use the solver to solve the specified problem, then run a simulation.
 
 This is designed to illustrate how solvers are expected to function. All solvers should be able to complete this standard test with the simple models in the POMDPModels package.
 
+Note that this does NOT test the optimality of the solution, but is only a smoke test to see if the solver interacts with POMDP models as expected.
+
 To run this with a solver called YourSolver, run
 ```
 using POMDPToolbox
@@ -71,7 +73,6 @@ using POMDPModels
 
 solver = YourSolver(# initialize with parameters #)
 test_solver(solver, BabyPOMDP())
-
 ```
 """
 function test_solver(solver::Solver, problem::POMDP; max_steps=10, updater=nothing)
